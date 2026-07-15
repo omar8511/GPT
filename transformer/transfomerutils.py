e91891ff89f88e79f0ae7e899f27e31723058993
Omar Bashir <ob324@ic.ac.uk>
2026-07-15 19:06:24 +0100

    Feat: (TransformerUtils) Add function to build shift by one pairs for training

---
+++ transformer/transfomerutils.py
@@ -0,0 +1,17 @@
+import torch
+
+def buildTrainingPairs(sequences: list[list[int]]) -> tuple[list[list[int]], list[list[int]]]:
+    """
+    Builds shift by one pairs for training
+
+    Args:
+    sequences: list of words encoded by BPE
+
+    Returns:
+    inputSeqs - list of words encoded by BPE dropping the last entry
+    targetSeqs - list of words encoded by BPE dropping the first entr
+    
+    """
+    inputSeqs = [seq[:-1] for seq in sequences]
+    targetSeqs = [seq[1:] for seq in sequences]
+    return inputSeqs, targetSeqs
